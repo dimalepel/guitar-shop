704c7cb86fe54ec01fc507dfa372491726faaadb
dimalepel <dimalepel@mail.ru>
2022-07-19 15:05:53 +0300

    feat: finish lesson 7

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,8 +1,37 @@
 class Products {
+  constructor() {
+    this.classNameActive = 'products-element__btn--active';
+    this.labelAdd = 'Добавить в корзину';
+    this.labelDelete = 'Удалить из корзины';
+  }
+
+  handleSetLocationStorage(element, id) {
+    const { pushProduct, products } = localStorageUtil.putProducts(id);
+
+    if (pushProduct) {
+      element.classList.add(this.classNameActive);
+      element.innerHTML = this.labelDelete;
+    } else {
+      element.classList.remove(this.classNameActive);
+      element.innerHTML = this.labelAdd;
+    }
+  }
+
   render() {
+    const productsStore = localStorageUtil.getProducts();
     let htmlCatalog = '';
 
     CATALOG.forEach(({ id, name, price, img}) => {
+      let activeClass = '';
+      let activeText = '';
+
+      if (productsStore.indexOf(id) === -1) {
+        activeText = this.labelAdd;
+      } else {
+        activeClass = this.classNameActive;
+        activeText = this.labelDelete;
+      }
+
       htmlCatalog += `
         <li class="products-element">
           <span class="products-element__name">${name}</span>
@@ -10,7 +39,7 @@ class Products {
           <span class="products-element__price">
             ⚡ ${price.toLocaleString()} USD
            </span>
-          <button class="products-element__btn" type="button">Добавить в корзину</button>
+          <button class="products-element__btn ${activeClass}" type="button" onclick="productsPage.handleSetLocationStorage(this, '${id}')">${activeText}</button>
         </li>
       `;
     });
